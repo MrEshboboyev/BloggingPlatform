78fa4c746d9ed2b69b37a428f9523b93733dbd3a
MrEshboboyev <mreshboboyev@gmail.com>
2024-07-11 21:31:23 +0500

    Inject Identity Classes in AccountController.cs

---
+++ BloggingPlatform/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    public class AccountController : Controller
+    {
+        // DI for IdentityClasses 
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
