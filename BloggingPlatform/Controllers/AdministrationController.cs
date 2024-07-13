87b4d460af4adf78635525ba31e1903429b324bf
MrEshboboyev <mreshboboyev@gmail.com>
2024-07-14 00:06:54 +0500

    Created AdministrationController.cs and DI for RoleManager<IdentityRole> ;

---
+++ BloggingPlatform/Controllers/AdministrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // DI : RoleManager
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+    }
+}
