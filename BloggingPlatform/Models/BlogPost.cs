d6b4ff8ba75bbe70a182584a457b88a5aacd0f95
MrEshboboyev <mreshboboyev@gmail.com>
2024-07-17 21:27:08 +0500

    Create BlogPost.cs -> Fields : Id, Title, Content, CreatedAt, UpdatedAt, AuthorId, virtual Author;

---
+++ BloggingPlatform/Models/BlogPost.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggingPlatform.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public string AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual IdentityUser Author { get; set; }
+    }
+}
