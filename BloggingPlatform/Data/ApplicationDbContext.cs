35057a0b2eb0a26c9ff4692c640c91859d09acf9
MrEshboboyev <mreshboboyev@gmail.com>
2024-07-17 16:04:36 +0500

    ApplicationDbContext.cs updated for ON DELETE NO ACTION; Adding Migrations;

--- BloggingPlatform/Data/ApplicationDbContext.cs
+++ BloggingPlatform/Data/ApplicationDbContext.cs
@@ -11,5 +11,21 @@ namespace BloggingPlatform.Data
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customizing the ASP.NET Identity model and overriding the defaults if needed
+            builder.Entity<IdentityUserRole<string>>()
+                   .HasOne<ApplicationRole>()
+                   .WithMany()
+                   .HasForeignKey(ur => ur.RoleId)
+                   .OnDelete(DeleteBehavior.NoAction);
+            //The following code will set ON DELETE NO ACTION to all Foreign Keys
+            //foreach (var foreignKey in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            //{
+            //    foreignKey.DeleteBehavior = DeleteBehavior.NoAction;
+            //}
+        }
     }
 }
